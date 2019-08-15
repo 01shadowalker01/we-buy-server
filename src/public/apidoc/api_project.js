bb3815c7b74a73a54d2b39a37a38253c54268819
saeed <s>
2019-08-16 00:33:18 +0430

    gitignore updated

--- src/public/apidoc/api_project.js
+++ src/public/apidoc/api_project.js
@@ -9,7 +9,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2019-08-15T10:39:35.613Z",
+    "time": "2019-08-15T15:33:32.147Z",
     "url": "http://apidocjs.com",
     "version": "0.17.7"
   }
